844d337888f932211c61e4348bdf798f9125d5de
milaso <562058113@qq.com>
2014-12-15 19:13:15 +0800

    框架搭建

---
+++ src/zo/milaso/mytetris/help.java
@@ -0,0 +1,11 @@
+package zo.milaso.mytetris;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class help extends Activity {
+	public void onCreate(Bundle saved){
+		super.onCreate(saved);
+		setContentView(R.layout.help);
+	}
+}
---
+++ src/zo/milaso/mytetris/setting.java
@@ -0,0 +1,11 @@
+package zo.milaso.mytetris;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class setting extends Activity {
+	public void onCreate(Bundle saved){
+		super.onCreate(saved);
+		setContentView(R.layout.setting);
+	}
+}
