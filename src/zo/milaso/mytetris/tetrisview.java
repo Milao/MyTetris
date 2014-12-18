979fe28bb6d794795ddaa49409ff407b790b2e6a
milaso <562058113@qq.com>
2014-12-18 15:14:02 +0800

    12.17

--- src/zo/milaso/mytetris/tetrisview.java
+++ src/zo/milaso/mytetris/tetrisview.java
@@ -1,13 +1,26 @@
 package zo.milaso.mytetris;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 
 public class tetrisview extends View {
+	public int Screen_width;
+	public int Screen_heigh;
 
 	public tetrisview(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
+	}
+
+	protected void onDraw(Canvas canvas) {
+		Bitmap bg = BitmapFactory.decodeResource(getResources(), R.drawable.bg);
+		canvas.drawBitmap(bg, 0, 0, null);
 	}
 
 }
